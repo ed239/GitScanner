5c8290ac5e47377e882f8878f7b484fb58ad4563
Eden Dubrovsky <ed239@njit.edu>
2024-06-04 12:06:19 -0400

    landing page + login route

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,14 @@
+'use client';
+import styles from './styles/Login.module.css';
+export default function login() {
+    return(
+        <div className={styles.page}>
+            <h1 className={styles.title}>Git Scanner</h1>
+            <div className={styles.box}>
+
+            </div>
+
+        </div>
+    );
+
+}
