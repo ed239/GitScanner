897cdfa2cd98ab989541c39f68f5efbcd5d79f03
Eden Dubrovsky <ed239@njit.edu>
2024-06-16 18:37:55 -0400

    login Page complete

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,14 +1,28 @@
 'use client';
+import { useState } from 'react';
 import styles from './styles/Login.module.css';
-export default function login() {
-    return(
+
+
+export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onLoginPressed = (e) => {
+        e.preventDefault();
+        console.log(username);
+        console.log(password);
+    };
+    return (
         <div className={styles.page}>
-            <h1 className={styles.title}>Git Scanner</h1>
             <div className={styles.box}>
-
+                <form onSubmit={onLoginPressed}>
+                    <h1 className={styles.title}>Git Scanner Login</h1>
+                    <input type="text" placeholder="Username" className={styles.inputField} value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    <input type="text" placeholder="Password" className={styles.inputField} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button type="submit" className={styles.button}>Login</button>
+                </form>
             </div>
-
         </div>
     );
-
 }
