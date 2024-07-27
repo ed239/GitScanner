cd7e179260b25161ca31f09ea585e76dae824987
Eden Dubrovsky <ed239@njit.edu>
2024-07-27 13:47:17 -0400

    added Navbar to login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,13 @@
 'use client';
 import { useState } from 'react';
 import styles from './styles/Login.module.css';
+import loginNavbar from './components/loginNavbar';
+import stylesLog from './components/loginNavbar.module.css';
+import { useRouter } from 'next/navigation';
 
 
 export default function Login() {
+    const router = useRouter();
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -13,8 +17,25 @@ export default function Login() {
         console.log(username);
         console.log(password);
     };
+
+    const navigateToHome = () => {
+        router.push('/');
+      };
     return (
+        <div className={styles.wholePage}>
+        <nav className={stylesLog.navbar}>
+            <ul className={stylesLog.navList}>
+                <li className={stylesLog.navItem}>
+                <button onClick={navigateToHome} className={stylesLog.loginButton}>
+                    Home
+                </button>
+                </li>
+            </ul>
+        </nav>
+        
         <div className={styles.page}>
+       
+        
             <div className={styles.box}>
                 <form onSubmit={onLoginPressed}>
                     <h1 className={styles.title}>Git Scanner Login</h1>
@@ -23,6 +44,7 @@ export default function Login() {
                     <button type="submit" className={styles.button}>Login</button>
                 </form>
             </div>
+        </div>
         </div>
     );
 }
