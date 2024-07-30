e9207f073c97af8b6728aeb0ec1d94c5f5a7c3f9
Eden Dubrovsky <ed239@njit.edu>
2024-07-29 21:18:13 -0400

    login,success,callback,navbar

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -7,44 +7,24 @@ import { useRouter } from 'next/navigation';
 
 
 export default function Login() {
-    const router = useRouter();
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const clientID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
     const onLoginPressed = (e) => {
         e.preventDefault();
-        console.log(username);
-        console.log(password);
+        //console.log(username);
+        //console.log(password);
+        console.log("Client ID: " + clientID);
+        window.location.assign("https://github.com/login/oauth/authorize?client_id=" + clientID)
     };
-
-    const navigateToHome = () => {
-        router.push('/');
-      };
     return (
-        <div className={styles.wholePage}>
-        <nav className={stylesLog.navbar}>
-            <ul className={stylesLog.navList}>
-                <li className={stylesLog.navItem}>
-                <button onClick={navigateToHome} className={stylesLog.loginButton}>
-                    Home
-                </button>
-                </li>
-            </ul>
-        </nav>
-        
         <div className={styles.page}>
-       
-        
             <div className={styles.box}>
                 <form onSubmit={onLoginPressed}>
                     <h1 className={styles.title}>Git Scanner Login</h1>
-                    <input type="text" placeholder="Username" className={styles.inputField} value={username} onChange={(e) => setUsername(e.target.value)}/>
-                    <input type="text" placeholder="Password" className={styles.inputField} value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <button type="submit" className={styles.button}>Login</button>
                 </form>
             </div>
-        </div>
         </div>
     );
 }
